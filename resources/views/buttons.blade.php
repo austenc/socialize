151310488a4fe4398112205b88435e6675e762ff
austenc <austen.cameron@gmail.com>
2020-06-14 15:46:38 -0600

    wip

--- resources/views/buttons.blade.php
+++ resources/views/buttons.blade.php
@@ -1,18 +1,19 @@
 <div class="socialize">
-    <div class="fixed left-0 transform translate-y-1/2">
-        <a class="socialize-btn text-white bg-blue-400" href="#twitter">
+    <div class="fixed left-0 transform -translate-y-1/2" style="top: 50%;">
+        <a class="socialize-btn text-white bg-social-twitter" href="#twitter">
+            <span class="transition-all hidden group-hover:inline-block duration-300">Twitter</span>
             @include('socialize::icons.twitter')
         </a>
-        <a class="socialize-btn bg-blue-200" href="#facebook">
+        <a class="socialize-btn text-white bg-social-facebook" href="#facebook">
             @include('socialize::icons.facebook')
         </a>
-        <a class="socialize-btn bg-blue-200" href="#instagram">
+        <a class="socialize-btn text-white bg-social-instagram" href="#instagram">
             @include('socialize::icons.instagram')
         </a>
-        <a class="socialize-btn bg-blue-200" href="#pinterest">
+        <a class="socialize-btn text-white bg-social-pinterest" href="#pinterest">
             @include('socialize::icons.pinterest')
         </a>
-        <a class="socialize-btn bg-blue-200" href="#email">
+        <a class="socialize-btn text-white bg-gray-900" href="#email">
             @include('socialize::icons.email')
         </a>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   purge: ['./resources/**/*', 'src/**/*.php'],
   theme: {
-    extend: {},
+    extend: {
+      scale: {
+        '115': '1.15',
+      },
+      colors: {
+        social: {
+          twitter: '#1da1f2',
+          facebook: '#1877f2',
+          instagram: '#c32aa3',
+          pinterest: '#bd081c',
+        },
+      },
+    },
   },
   variants: {},
   plugins: [],

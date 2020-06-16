c0dea83953439faaf2f80419757d22755bc806d5
austenc <austen.cameron@gmail.com>
2020-06-15 22:38:47 -0600

    wip

--- src/Blueprints/SocialProviderBlueprint.php
+++ src/Blueprints/SocialProviderBlueprint.php
@@ -21,28 +21,54 @@ class SocialProviderBlueprint
                             ]
                         ],
                         [
-                            'handle' => 'twitter_message',
+                            'handle' => 'twitter_url',
                             'field'  => [
-                                'type'     => 'text',
-                                'width'    => '75',
-                                'display'  => 'Default message / hashtags in the sharing popup',
-                                'validate' => 'required',
+                                'type' => 'text',
+                                'width' => '75',
+                                'display' => 'URL',
+                                'instructions'  => 'URL included with the tweet. Leave blank to use current page URL',
+                                // 'validate' => 'required',
+                            ],
+                        ],
+                        [
+                            'handle' => 'twitter_text',
+                            'field'  => [
+                                'type' => 'textarea',
+                                'display' => 'Text',
+                                'instructions'  => 'Pre-populated text highlighted in the tweet composer',
+                            ],
+                        ],
+                        [
+                            'handle' => 'twitter_hashtags',
+                            'field'  => [
+                                'type' => 'text',
+                                'display' => 'Hashtags',
+                                'instructions'  => 'A comma-separated list of hashtags to be appended to default tweet text',
+                            ],
+                        ],
+                        [
+                            'handle' => 'twitter_related',
+                            'field'  => [
+                                'type' => 'text',
+                                'display' => 'Related Accounts',
+                                'instructions'  => 'A comma-separated list of accounts related to the content of the shared URI',
+                                // 'validate' => 'required',
                             ],
                         ],
                     ],
                 ],
-                'Instagram' => [
-                    'fields' => [
-                        [
-                            'handle' => 'instagram_enabled',
-                            'field' => [
-                                'display' => 'Button Enabled',
-                                'type' => 'toggle',
-                                'width' => '25'
-                            ]
-                        ],
-                    ]
-                ],
+                // 'Instagram' => [
+                //     'fields' => [
+                //         [
+                //             'handle' => 'instagram_enabled',
+                //             'field' => [
+                //                 'display' => 'Button Enabled',
+                //                 'type' => 'toggle',
+                //                 'width' => '25'
+                //             ]
+                //         ],
+                //     ]
+                // ],
                 'Facebook' => [
                     'fields' => [
                         [
@@ -76,6 +102,25 @@ class SocialProviderBlueprint
                                 'type' => 'toggle',
                                 'width' => '25'
                             ]
+                        ],
+                        [
+                            'handle' => 'email_subject',
+                            'field'  => [
+                                'type' => 'text',
+                                'display' => 'Subject',
+                                'width' => '75',
+                                'instructions'  => 'The default subject line of the email',
+                                // 'validate' => 'required',
+                            ],
+                        ],
+                        [
+                            'handle' => 'email_text',
+                            'field'  => [
+                                'type' => 'textarea',
+                                'display' => 'Body',
+                                'instructions'  => 'Text contents of the email',
+                                // 'validate' => 'required',
+                            ],
                         ],
                     ]
                 ],

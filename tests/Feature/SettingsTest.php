c967c7834c2e676415277cfdfa75272d12957e5b
austenc <austen.cameron@gmail.com>
2020-06-17 07:18:47 -0600

    wip

---
+++ tests/Feature/SettingsTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Austenc\Socialize\Tests;
+
+class SettingsTest extends TestCase
+{
+    public function test_can_store_settings()
+    {
+        // Login as CP user / make one
+        // $this->actingAs()
+
+        // Post to the route
+        $this->assertTrue(true);
+
+        // Assert that there is data
+    }
+}
